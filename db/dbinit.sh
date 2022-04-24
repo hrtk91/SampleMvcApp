5d806cd8a9d0a811f9c9c821dadc03b23550ec56
hrtk91 <ht91218@gmail.com>
2022-04-24 21:48:49 +0900

    Docker修正

--- db/dbinit.sh
+++ db/dbinit.sh
@@ -3,7 +3,8 @@ echo "executing createuser.sql"
 retryCount=0
 until sqlcmd -S localhost,1433 -U SA -P Abcd1234# -i /tmp/createuser.sql
 do
-    if [retryCount++ -gt 5]; then
+    retryCount=$(($retryCount + 1))
+    if [retryCount -gt 5]; then
         exit 1
     fi
     sleep 1s
